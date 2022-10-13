f6b2f79007ba0618e69d41feb5e11ee1ecd84815
sunaina134 <107124879+sunaina134@users.noreply.github.com>
2022-10-13 14:12:49 +0530

    first commit

---
+++ Iterators.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+int main(){
+    vector<int> v={10,20,30,40,50};
+    vector<int>::iterator i=v.begin();
+    cout<<(*i)<<" ";
+    i++;
+    cout<<(*i)<<" ";
+    i=next(i);
+     cout<<(*i)<<" ";
+
+    i=v.end();
+i--;
+cout<<(*i)<<(*i)<<"";
+return 0;
+}
+
+
+
+// | | | | |
+// b         e
+
+// begin 
+// end 
+// prev
+// next 
+// advance
+
+
+
+
+//member access iterator to vector random access, big(o);
+//pointer airthmetic we can do
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # CPP-STL
 CPP Standard Template Library Practice
+Day1--
+Iterators
+Simple 
+forward_list--> Forward iterator
+list -->Bidirectional iterators
+vector--> Random iterators
+
+Associate
+set-->Bidirectional iterators
+map-->Bidirectional iterators
+multimap-->Bidirectional iterators
+multiset-->Bidirectional iterators
+unordered set-->forward
+unordered map-->forward
+
+Adapters
+Queue,Stack ,Priority Queue--->dont implement iterators
